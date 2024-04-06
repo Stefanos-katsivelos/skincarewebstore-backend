a4bd954ae89c4fd9a54681d09053480c1ef3706c
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-04-06 08:49:52 +0300

    git ingore and mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const  Mongoose = require('mongoose');
 const server = express();
 const port = 3000;
+const mongoose = require('mongoose')
 
 
-Mongoose.connect("MONGODB_URI = mongodb+srv://cfuser:1234567890@cluster0.myyxuoc.mongodb.net/?retryWrites=true&w=majority&SkincareStore=Cluster0");
+mongoose.connect("process.env.MONGODB_URI")
+  .then( 
+    () => {console.log("Connection to mognoDB established")},
+    err => {console.log("Failed to connect to mongoDB", err)}
+  );
